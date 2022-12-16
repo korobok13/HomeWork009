6fa76682ba44fc99a96339f745da3a9609e89a09
korobov_o <korobok13@yandex.ru>
2022-12-16 12:37:34 +0300

    третья задача

---
+++ HomeWork093/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+int Ackerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+
+    }
+    else if ((n != 0) && (m == 0))
+    {
+        return Ackerman(n - 1, 1);
+    }
+    Console.WriteLine($"Ackerman({n}, {m}) -> {Ackerman(n - 1, +Ackerman(n, m - 1))}");
+    return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Ackerman(a, b));
